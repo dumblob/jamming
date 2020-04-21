32ef35d06c8db40cf47c3d0a5fbbfe6ca88d2000
John-Mark Gurney <jmg@funkthat.com>
2020-04-20 21:54:13 -0700

    add support for stubbing out getUserMedia for now..

--- src/index/index.spec.js
+++ src/index/index.spec.js
@@ -1,12 +1,38 @@
 'use strict'
 
+const sinon = require('sinon')
 const index = require('../index')
 const expect = require('chai').expect
+
+var navigator;
+
+function setupBrowser() {
+	navigator = {
+		mediaDevices: {
+			getUserMedia: () => { console.log('dummy'); throw 'failure'; }
+		}
+	}
+	global.navigator = navigator;
+}
 
 describe('index module', () => {
 	describe('"members"', () => {
 		it('should be have the following members', () => {
 			expect(global.runPage).to.be.a('function');
+		})
+	})
+	describe('"runPage"', () => {
+		it('should call getUserMedia', async () => {
+			setupBrowser();
+
+			var gum = sinon.stub(navigator.mediaDevices, 'getUserMedia');
+			gum.onFirstCall().returns(true);
+
+			await global.runPage();
+
+			expect(gum.calledOnce).to.be.true;
+
+			gum.restore();
 		})
 	})
 })
